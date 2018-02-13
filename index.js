e6dd03d0ecaeb956056556c5904ca3c05e5ba4e1
niranjan-Infofaces <niranjand@infofaces.co.in>
2018-02-13 15:51:16 +0530

    Keys added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const requestAPI = require('request-promise');
-
+const API_KEYS = require('./Key/API_KEY')
 const server = express();
 server.use(bodyParser.urlencoded({
     extended: true
@@ -11,43 +11,75 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 var dataToSend = "";
+let action = "";
 server.post('/', function (request, response) {
-let action = request.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
-let parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
-let inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
-let requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
-let _Query =request.body.result.resolvedQuery;
-console.log("action: "+action);
-console.log("inputContexts: "+inputContexts);
-console.log("parameters :"+parameters);
-console.log("requestSource :"+requestSource);
+    ProcessRequest(request, response)
 });
-server.post('/getUberDetails', function (req, res) {
-    let _query ='';
-    let movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.location ? _query=req.body.result.parameters.location : _query=req.body.result.parameters.location ;
-    console.log(_query);
-    let reqUrl = encodeURI('http://api.openweathermap.org/data/2.5/weather?q=' + _query + '&appid=c8608c37f0247f5a563aa6b7cc7d0dac');
+function ProcessRequest(req, res) {
+    action = req.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
+    let parameters = req.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
+    let inputContexts = req.body.result.contexts; // https://dialogflow.com/docs/contexts
+    let requestSource = (req.body.originalRequest) ? req.body.originalRequest.source : undefined;
+    let _Query = req.body.result.resolvedQuery;
+    //console.log("action: "+action);
+    console.log("inputContexts: " + inputContexts);
+    console.log("parameters :" + JSON.stringify(parameters));
+    console.log("requestSource :" + requestSource);
+    const actionHandlers = {
+        'getUberDetails': () => {
+            let _query = '';
+            _query = req.body.result.parameters.location;
+            console.log(_query);
+            let reqUrl = encodeURI('http://api.openweathermap.org/data/2.5/weather?q=' + _query + '&appid=c8608c37f0247f5a563aa6b7cc7d0dac');
 
-    requestAPI(reqUrl)
-        .then(function (data) {
-              console.log( data)
-            res.json({
-                speech: "data",
-                displayText: data[0].coord,
-                source: 'getUberDetails'
-            });
-        })
-        .catch(function (err) {
-            console.error(err);
-            return res.json({
-                speech: err,
-                displayText: 'Something went wrong!',
-                source: 'getUberDetails'
-            });
-        })
-    
-});
+            requestAPI(reqUrl)
+                .then(function (data) {
+                    console.log(data)
+                    res.json({
+                        speech: "data",
+                        displayText: data[0].coord,
+                        source: 'getUberDetails'
+                    });
+                })
+                .catch(function (err) {
+                    console.error(err);
+                    return res.json({
+                        speech: err,
+                        displayText: 'Something went wrong!',
+                        source: 'getUberDetails'
+                    });
+                })
 
+        },
+        'getProductDetails': () => {
+            let Url = 'https://api.uber.com/v1.2/products?';    // #1
+            let latitude = '13.0827';                           // #2
+            let longitude = '80.2707'                           // #3
+            let token = API_KEYS.UBER_API_KEYS.SERVER_TOKEN;    // #4
+            //https://api.uber.com/v1.2/products?latitude=13.0827&longitude=80.2707&server_token=9q6z4n75sEhTzekU0eEXIo8_e1yePXOZ47SSVEeD
+            let reqUrl = encodeURI(Url + 'latitude=' + latitude + '&' + 'longitude=' + longitude + "&" + "server_token=" + token)
+            console.log(reqUrl);
+            requestAPI(reqUrl)
+                .then(function (data) {
+                    let _productsData = JSON.parse(data);
+                    var _DisplayName = [];
+                    var _productsCount = Object.keys(_productsData.products).length;
+                    for (var i = 0; i < _productsCount; i++) {
+                        _DisplayName.push(_productsData.products[i].display_name)
+                    }
+                    res.json({ data: _DisplayName.toString() })
+                })
+                .catch(function (err) {
+                    console.error(err);
+                    res.json({ Error: err })
+                })
+        }
+    };
+    if (!actionHandlers[action]) {
+        action = 'default';
+    }
+    actionHandlers[action]();
+}
 server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and running...");
 });
